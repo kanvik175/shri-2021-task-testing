b711fa168a38b93d4fbfa7a5c5be91bd7a2bd1e8
rus <sr@trinitydigital.ru>
2021-11-06 17:39:53 +0300

    fix: hermione config

--- .hermione.conf.js
+++ .hermione.conf.js
@@ -1,6 +1,6 @@
 module.exports = {
     retry: 2,
-    baseUrl: "https://shri.yandex",
+    baseUrl: "https://shri.yandex/hw/store",
     gridUrl: "http://127.0.0.1:4444/wd/hub",
     browsers: {
         chrome: {
