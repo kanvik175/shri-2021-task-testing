74446a228beebe7e1d259050d32b453b390faa08
rus <sr@trinitydigital.ru>
2021-11-06 17:50:24 +0300

    fix: hermion tests

--- test/hermione/common.hermione.js
+++ test/hermione/common.hermione.js
@@ -1,20 +1,22 @@
 const { assert } = require("chai");
 
-it("на ширине меньше 576px навигационное меню должно скрываться за 'гамбургер'", async () => {
-  await this.browser.url("/hw/store");
-  await this.browser.setWindowSize(320, 568);
-  const nav = await this.browser.$('.navbar-collapse');
+describe("общие", async function() {
+  it("на ширине меньше 576px навигационное меню должно скрываться за 'гамбургер'", async function() {
+    await this.browser.url("/hw/store");
+    await this.browser.setWindowSize(320, 568);
+    const nav = await this.browser.$('.navbar-collapse');
 
-  assert.isFalse(await nav.isDisplayed())
-});
+    assert.isFalse(await nav.isDisplayed())
+  });
 
-it("при выборе элемента из меню 'гамбургера', меню должно закрываться", async () => {
-  await this.browser.url("/hw/store");
-  await this.browser.setWindowSize(320, 568);
-  const nav = await this.browser.$('.navbar-collapse');
-  const toggle = await this.browser.$('.navbar-toggler');
+  it("при выборе элемента из меню 'гамбургера', меню должно закрываться", async function() {
+    await this.browser.url("/hw/store");
+    await this.browser.setWindowSize(320, 568);
+    const nav = await this.browser.$('.navbar-collapse');
+    const toggle = await this.browser.$('.navbar-toggler');
 
-  toggle.click();
+    toggle.click();
 
-  assert.isTrue(await nav.isDisplayed())
-});
+    assert.isTrue(await nav.isDisplayed())
+  });
+})
